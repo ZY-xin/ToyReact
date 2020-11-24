2f57c8f00fa71c7ada060039a2a0a78bf98e65ef
“dubaixing” <dubaixing@uniontech.com>
2020-11-24 18:50:22 +0800

    finish today"s homework

--- main.js
+++ main.js
@@ -1,17 +1,68 @@
-import { Component, render } from './toy-react.js'
+
+
+import './toy-react.js'
 
 class MyComponent extends Bue.jsxComp {
-    render() {
-        return <div title="test title" class="bx-test abc" style="border:solid 1px red;">{this.children}</div>
+  constructor() {
+    super()
+    this.state = {
+      foo: 1234567
     }
+  }
+  changeValueOfFoo() {
+    this.setState({
+      foo: "foo:" + Math.random()
+    })
+  }
+  render() {
+    return <div title="test title" class="bx-test abc"
+      style="border:solid 1px red;">
+      <button onClick={this.changeValueOfFoo.bind(this)}>change state</button>
+      <div style="background:rgba(0,0,0,0.5)">{this.state.foo}</div>
+      {this.children}</div>
+
+
+
+
+    /**
+     * the jsx above is same as:
+     * 
+     * return Bue.createElement(
+
+          //tag name
+          "div",
+          //props
+          {
+            title: "test title",
+            "class": "bx-test abc",
+            style: "border:solid 1px red;"
+          },
+          //self children
+          Bue.createElement(
+            "div",
+            {
+              style: "background:rgba(255,255,255,0.5)"
+
+            },
+            //it is a child,but it's textNode child,value from state
+            this.state.foo),
+
+          //children in render method first arg
+          this.children
+
+        );
+     */
+
+
+  }
 }
 
 
 
 window.addEventListener('load', () => {
-    Bue.render(<MyComponent>
-        i am pure text
+  Bue.render(<MyComponent>
+    i am pure text
         <div title="a">a</div>
-        {/* <div title="b">b</div> */}
-    </MyComponent>, document.body)
+    {/* <div title="b">b</div> */}
+  </MyComponent>, document.body)
 })
--- toy-react.js
+++ toy-react.js
@@ -1,8 +1,12 @@
 
-
+const RENDER_TO_DOM = Symbol("render to dom");
 class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content)
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
@@ -12,43 +16,81 @@ class ElementWrapper {
         this.root = document.createElement(tagName)
     }
 
-    setAttribute(name, value) {
-        this.root.setAttribute(name, value)
+
+    appendChild(component) {
+        let range = document.createRange();
+        range.setStart(this.root, this.root.childNodes.length);
+        range.setEnd(this.root, this.root.childNodes.length);
+        console.log('component in append child--:', component)
+        component[RENDER_TO_DOM](range);
     }
-    appendChild(child) {
-        if (child.root) {
-            this.root.appendChild(child.root)
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
+    }
+    setAttribute(name, value) {
+        if (name.match(/^on([\s\S]+)$/)) {
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value);
+        } else if (name === 'className') {
+            this.root.setAttribute('class', value);
         }
+        else {
+            this.root.setAttribute(name, value)
+        }
+
     }
 }
 
 
 
-function insertChild(child, parent) {
-    console.log('childdddddddddddddd---:', child)
-    if (
-        typeof child == 'object' &&
-        child instanceof Array
-    ) {
-        for (let c of child) {
-            insertChild(c, parent)
+function insertChildren(children, parent) {
+
+    children = [].concat(children)
+    console.log('children in insert children--:', children)
+
+    for (let child of children) {
+        console.log('childdddddddddddddd---:', child)
+        if (
+            typeof child == 'object' &&
+            child instanceof Array
+        ) {
+            for (let c of child) {
+                insertChildren(c, parent)
+            }
         }
+        if (typeof child == 'number') {
+            child = child.toString()
+        }
+        if (typeof child === 'string') {
+            child = new TextWrapper(child)
+        }
+        parent.appendChild(child)
     }
-    if (typeof child === 'string') {
-        child = new TextWrapper(child)
-    }
-    parent.appendChild(child)
+
+
+
 }
 
 
 let Bue = function () {
 
 }
+
+let merge = (oldState, newState) => {
+    for (let p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== "object") {
+            oldState[p] = newState[p];
+        } else {
+            merge(oldState[p], newState[p]);
+        }
+    }
+}
 Bue.jsxComp = class {
     constructor() {
-        this._root = null
-        this.props = Object.create(null)
-        this.children = []
+        this.props = Object.create(null);
+        this.children = [];
+        this._root = null;
+        this._range = null;
     }
     setAttribute(name, value) {
         this.props[name] = value    // props还未实现
@@ -56,19 +98,47 @@ Bue.jsxComp = class {
     appendChild(child) {    // component 添加 
         this.children.push(child)
     }
-    get root() {
-        console.log('root was get ed--:', this._root)
-        // console.log('this---:', this)
-        if (!this._root) {
-            console.log('render in this--:', this.render())
-            //this.render是一个继承了Bue.createJsxComp的被实例化了的类,且有render方法,
-            //render方法调用,实际上是调用createElement方法,
-            //然后再访问其root属性,root属性为一个真实d的dom node
+    //root是一个和渲染相关的东西
+    //重新渲染  = 重新设置root  + 重新append?
+    // get root() {
+    //     console.log('root was get ed--:', this._root)
+    //     // console.log('this---:', this)
+    //     if (!this._root) {
+    //         console.log('render in this--:', this.render())
+    //         //this.render是一个继承了Bue.createJsxComp的被实例化了的类,且有render方法,
+    //         //render方法调用,实际上是调用createElement方法,
+    //         //然后再访问其root属性,root属性为一个真实d的dom node
 
-            //render方法又会调用ElementWrapper方法,该class在constructor阶段会生成root属性,所以此处可以访问到root属性
-            this._root = this.render().root
+    //         //render方法又会调用ElementWrapper方法,该class在constructor阶段会生成root属性,
+    //         // 所以此处可以访问到root属性
+    //         this._root = this.render().root
+    //     }
+    //     return this._root
+    // }
+    setState(newState) {
+        if (this.state === null || typeof this.state !== "object") {
+            this.state = newState;
+            this.rerender();
+            return;
         }
-        return this._root
+
+        merge(this.state, newState);
+        console.log('will re render')
+        this.rerender();
+    }
+    rerender() {
+        let oldRange = this._range;
+        let range = document.createRange();
+        range.setStart(oldRange.startContainer, oldRange.startOffset);
+        range.setEnd(oldRange.startContainer, oldRange.startOffset);
+        this[RENDER_TO_DOM](range);
+
+        oldRange.setStart(range.endContainer, range.endOffset)
+        oldRange.deleteContents();
+    }
+    [RENDER_TO_DOM](range) {
+        this._range = range;
+        this.render()[RENDER_TO_DOM](range);
     }
 }
 
@@ -98,7 +168,7 @@ function createElement(type, attributes, ...children) {
     }
     if (children) {
         for (let child of children) {
-            insertChild(child, e)
+            insertChildren(child, e)
         }
     }
     return e
@@ -110,8 +180,10 @@ function createElement(type, attributes, ...children) {
 //     mountEle.appendChild(component.root);
 // }
 
-Bue.render = (component, mountEle) => {
-
-    console.log('component.root---:', component.root)
-    mountEle.appendChild(component.root);
+Bue.render = function (component, parentElement) {
+    let range = document.createRange();
+    range.setStart(parentElement, 1);
+    range.setEnd(parentElement, parentElement.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
 }
